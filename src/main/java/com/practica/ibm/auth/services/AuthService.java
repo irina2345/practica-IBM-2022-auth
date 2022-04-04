a1dc3c08894de78c234b416746c6748512e44c76
evamontejano <evamontejano30@gmail.com>
2022-04-04 19:28:38 +0300

    tema

--- src/main/java/com/practica/ibm/auth/services/AuthService.java
+++ src/main/java/com/practica/ibm/auth/services/AuthService.java
@@ -30,6 +30,11 @@ public class AuthService {
         // exista deja un cont cu acest mail? daca da adauga in lista de erori
         // Optional<Account> optionalAccount = accountRepository.getAccountByEmail();
         // optionalAccount.isPresent();
+        Optional<Account> optionalAccount = accountRepository.getAccountByEmail(signupRequestBody.getEmail());
+
+        if(optionalAccount.isPresent()) {
+            errors.add("Emailul a fost gasit deja!");
+        }
 
         Account account = new Account();
         account.setEmail(signupRequestBody.getEmail());
